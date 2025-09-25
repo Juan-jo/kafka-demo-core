464e24681473cef581c622113a7f9f2fc7d26b56
jdev <juanj@jdev.com>
2025-09-24 20:23:16 -0600

    init

---
+++ src/main/java/com/jdev/kafka/core/KafkaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jdev.kafka.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCoreApplication.class, args);
+	}
+
+}
