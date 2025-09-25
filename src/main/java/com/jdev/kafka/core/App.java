216d05bc8668abed176f4967c6050b2aa929062d
jdev <juanj@jdev.com>
2025-09-24 20:07:31 -0600

    init

---
+++ src/main/java/com/jdev/kafka/core/App.java
@@ -0,0 +1,13 @@
+package com.jdev.kafka.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
